d0ab5833b6510df241d597f30311479ea6cc4f49
NiramayThaker <thakerniramay@gmail.com>
2025-01-17 16:30:45 +0530

    Deleting file from local server when deleted from database

--- core/models.py
+++ core/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Dog(models.Model):
 	name = models.CharField(max_length=64)
 	image = models.ImageField(upload_to='dogs/')
 
+	# Can use django-clean lib.
+	def delete(self):
+		self.image.delete()
+		super().delete()
