630c7534d5a68f3e148b3c312780f05e6bfaac6a
NiramayThaker <thakerniramay@gmail.com>
2025-01-18 09:12:30 +0530

    Adding file type restriction while uploading

--- core/models.py
+++ core/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
+
+ext_validator = FileExtensionValidator(['png', 'jpg', 'pdf'])
 
 
 # Create your models here.
 class Dog(models.Model):
 	name = models.CharField(max_length=64)
-	image = models.ImageField(upload_to='dogs/')
+	image = models.FileField(upload_to='dogs/', validators=[ext_validator])
 
 	# Can use django-clean lib.
 	def delete(self):
